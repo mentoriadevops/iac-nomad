c5b94f4b961eb5b22dad407a5b74e19ebd7e413b
Gabriel Iglesias <ga12iglesias@gmail.com>
2021-11-10 21:48:00 -0300

    refactor: Realizando alterações conforme comentários no PR.
    Retiramos o .tfvars e alteramos para local.tf.
    Atualizamos para a release v0.2.0 do módulo Compute GCP.
    Alteramos o for_each dos clients para count.
    Atualizamos e testamos a imagem gerada pelo módulo Packer.
    Alteramos o nome dos servers para each.key (nome da instância = chave)
    
    Signed-off-by: Gabriel Iglesias <ga12iglesias@gmail.com>
    Co-Authored-by: Danilo Figueiredo Rocha <snifbr@gmail.com>
    Co-Authored-by: Luiz Aoqui <lgfa29@gmail.com>
    Co-authored-by: Felipe Nobrega <lipenodias@gmail.com>
    Co-authored-by: Marcelo Mansur <mansur.ufmg@gmail.com>

--- main.tf
+++ main.tf
@@ -31,43 +31,60 @@ module "network_gcp" {
 }
 
 module "nomad_servers" {
-  source = "github.com/mentoriaiac/iac-modulo-compute-gcp.git?ref=6ce607c"
+  source = "github.com/mentoriaiac/iac-modulo-compute-gcp.git?ref=v0.2.0"
 
-  for_each = var.server_pool
+  for_each = local.server_pool
 
   project                 = var.project
-  instance_name           = each.value.instance_name
+  instance_name           = each.key
   instance_image          = each.value.instance_image
   machine_type            = each.value.machine_type
   zone                    = each.value.zone
   network                 = module.network_gcp.vpc_id
   subnetwork              = module.network_gcp.subnets[0].id
-  tags                    = each.value.tag
-  metadata_startup_script = each.value.metadata_startup_script
-  labels                  = each.value.labels
-
+  metadata_startup_script = <<EOF
+  /usr/local/bin/nomad_bootstrap.sh server 3 '\"provider=gce project_name=${var.project} tag_value=nomad-server\"'
+  EOF 
+  tags                    = ["nomad", "nomad-server"]
+  labels = {
+    terraform = "true",
+    component = "nomad_server"
+  }
   service_account_scopes = [
     "https://www.googleapis.com/auth/compute.readonly",
   ]
+}
+
+
+data "google_compute_zones" "us-central1" {
+  #  project = mentoria-terraform 
+  region = "us-central1"
 }
 
 module "nomad_clients" {
-  source = "github.com/mentoriaiac/iac-modulo-compute-gcp.git?ref=6ce607c"
+  source = "github.com/mentoriaiac/iac-modulo-compute-gcp.git?ref=v0.2.0"
 
-  for_each = var.client_pool
+  count = 3
 
   project                 = var.project
-  instance_name           = each.value.instance_name
-  instance_image          = each.value.instance_image
-  machine_type            = each.value.machine_type
-  zone                    = each.value.zone
+  instance_name           = "client-${count.index + 1}"
+  machine_type            = "e2-medium"
+  instance_image          = "orquestradores-20211110235155"
+  zone                    = data.google_compute_zones.us-central1.names[count.index % length(data.google_compute_zones.us-central1.names)]
   network                 = module.network_gcp.vpc_id
   subnetwork              = module.network_gcp.subnets[0].id
-  tags                    = each.value.tag
-  metadata_startup_script = each.value.metadata_startup_script
-  labels                  = each.value.labels
+  metadata_startup_script = <<EOF
+  /usr/local/bin/nomad_bootstrap.sh client '\"provider=gce project_name=${var.project} tag_value=nomad-server\"'
+  EOF
 
   service_account_scopes = [
     "https://www.googleapis.com/auth/compute.readonly",
   ]
+
+  tags = ["nomad"]
+  labels = {
+    terraform = "true",
+    component = "nomad_server"
+  }
 }
+
--- variables.tf
+++ variables.tf
@@ -9,29 +9,3 @@ variable "region" {
   default     = "us-central1"
 }
 
-# A decisão em utilizar a variável instance_name ao invés da key se deu no sentido de facilitar a leitura dos nomes e tipos das instâncias (server/client) para todos os níveis de utilizadores.
-variable "server_pool" {
-  description = "Quantidade de Máquinas Servers"
-  type = map(object({
-    instance_name           = string
-    machine_type            = string
-    instance_image          = string
-    zone                    = string
-    metadata_startup_script = string
-    tag                     = list(string)
-    labels                  = map(string)
-  }))
-}
-
-variable "client_pool" {
-  description = "Quantidade de Máquinas Clients"
-  type = map(object({
-    instance_name           = string
-    machine_type            = string
-    instance_image          = string
-    zone                    = string
-    metadata_startup_script = string
-    tag                     = list(string)
-    labels                  = map(string)
-  }))
-}
